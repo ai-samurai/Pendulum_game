4cd1a342f9383ab1b865cb582d95227dffcc193f
ai-samurai <tanmay.inglay@gmail.com>
2021-08-05 02:02:25 +0530

    fixing .gitignore

--- scripts/background.gd
+++
@@ -1,9 +0,0 @@
-extends ParallaxBackground
- 
-export var camera_velocity: Vector2 = Vector2( 0, 100);
- 
- 
-func _process(delta: float) -> void:
-	camera_velocity = Vector2(0, gv.background_speed)
-	var new_offset: Vector2 = get_scroll_offset() + camera_velocity * delta
-	set_scroll_offset( new_offset )
